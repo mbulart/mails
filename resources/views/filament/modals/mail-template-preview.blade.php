dfb18a51bf7df5f2806601b535833549a9952cd4
Joseph MBULA <josephmbula5994@gmail.com>
2026-05-12 10:48:55 +0100

    ok

--- resources/views/filament/modals/mail-template-preview.blade.php
+++ resources/views/filament/modals/mail-template-preview.blade.php
@@ -9,15 +9,15 @@
     $previewSrc = 'data:text/html;charset=utf-8;base64,'.base64_encode($previewHtml);
 @endphp
 
-<div class="space-y-4">
+<div class="not-prose w-full max-w-none space-y-4">
     <div>
         <p class="text-sm text-gray-500">Sujet</p>
         <p class="font-medium">{{ $subject }}</p>
     </div>
 
-    <div class="rounded-lg border border-gray-200 bg-white">
+    <div class="w-full rounded-lg border border-gray-200 bg-white">
         <iframe
-            class="h-[70vh] w-full rounded-lg"
+            class="block h-[70vh] w-full rounded-lg"
             sandbox="allow-same-origin allow-scripts"
             src="{{ $previewSrc }}"
             title="Prévisualisation HTML du template"
